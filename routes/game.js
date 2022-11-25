051fa8e19d5a2e8d484c2f471e9583fa1516c484
Tanaka <f.tanaka@gftd.works>
2022-11-25 14:53:46 +0900

    ベースとなるページを作成

---
+++ routes/game.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config');
+
+router.get('/', (req, res, next) => {
+  let displayName = 'anonymous';
+  let thumbUrl = 'anonymous';
+  if(req.user){
+    displayName = req.user.displayName;
+    thumbUrl = req.user.photos[0].value;
+  }
+  res.render('game', {title:'潜水艦ゲーム', displayName: displayName, thumbUrl: thumbUrl, ipAddress: config.ipAddress});
+});
+
+module.exports = router;
