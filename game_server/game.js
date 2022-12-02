cf05b6cc399c498c7bf035cbdfd14995a553a20c
Tanaka <f.tanaka@gftd.works>
2022-12-02 14:32:02 +0900

    マップ情報、アイテム情報などを作成する関数を追加（サーバーサイド）

---
+++ game_server/game.js
@@ -0,0 +1,173 @@
+'use strict';
+
+//ハッシュ計算用ライブラリの読み込み
+const crypto = require('crypto');
+
+
+//ゲームで使用する値を格納するオブジェクト
+/* ゲームに設定で必要になった値は、このgameObjに設定していく */
+const gameObj = {
+  playersMap: new Map(),  //ゲームに参加しているプレイヤーの情報を入れておく連想配列
+  itemsMap: new Map(),  //ミサイル（魚雷）のアイテム情報を入れておく連想配列
+  airMap: new Map(),  //酸素のアイテム情報を入れておく連想配列
+  fieldWidth: 1000,  //ゲームの横幅
+  fieldHeight: 1000,  //ゲームの横幅
+  itemTotal: 15,  //ゲームに出現するミサイルのアイテム数
+  airTotal: 10  //ゲームに出現する酸素のアイテム数
+};
+
+
+//ゲームの初期設定をする関数
+function init() {
+  for(let i = 0; i < gameObj.itemTotal; i++){
+    addItem();
+  };
+  for(let a = 0; a < gameObj.airTotal; a++){
+    addAir();
+  };
+};
+
+init();  //ゲームの初期化（初期化はサーバ起動時に行う）
+
+
+//新しい接続を作り、ユーザをゲームに参加させ、ゲームの現在の状態を返す
+function newConnection(socketId, displayName, thumbUrl) {
+
+  //プレイヤー（潜水艦）の初期座標を乱数で設定
+  const playerX = Math.floor(Math.random() * gameObj.fieldWidth);
+  const playerY = Math.floor(Math.random() * gameObj.fieldHeight);
+
+  //プレイヤーのIDを、socketIdをもとにハッシュ関数で生成
+  const playerId = crypto.createHash('sha1').update(socketId).digest('hex');
+
+  //プレイヤーデータを持ったオブジェクト
+  const playerObj = {
+    x: playerX,
+    y: playerY,
+    playerId: playerId,
+    displayName: displayName,
+    thumbUrl: thumbUrl,
+    isAlive: true,  //生存フラグ（初期状態は生存なのでtrue）
+    direction: 'right',  //潜水艦の進行方向（初期は右向き）
+    score: 0  //得点（初期は0点）
+  };
+
+  //socketIdとplayerObjを、gameObj.playerMap連想配列に追加
+  gameObj.playersMap.set(socketId, playerObj);
+
+  //ゲームの設定などをオブジェクトに入れて、リターンで返す
+  const startObj = {
+    playerObj: playerObj,
+    fieldWidth: gameObj.fieldWidth,
+    fieldHeight: gameObj.fieldHeight
+  };
+  return startObj;
+};
+
+
+//マップ情報を作成して返す関数
+/* 
+  gameObj.playersMapとgameObj.itemsMap、gameObj.airMapを返すだけだが、
+  オブジェクトのままだと通信で送るにはデータとして大きすぎるので、
+  値だけを配列に入れて返すようにする。
+*/
+function getMapData() {
+  const playersArray = [];
+  const itemsArray = [];
+  const airArray = [];
+
+  for(let [socketId, player] of gameObj.playersMap) {
+    const playerDataForSend = [];
+
+    playerDataForSend.push(player.x);
+    playerDataForSend.push(player.y);
+    playerDataForSend.push(player.playerId);
+    playerDataForSend.push(player.displayName);
+    playerDataForSend.push(player.score);
+    playerDataForSend.push(player.isAlive);
+    playerDataForSend.push(player.direction);
+
+    playersArray.push(playerDataForSend);
+  };
+
+  for(let [id, item] of gameObj.itemsMap) {
+    const itemDataForSend = [];
+
+    itemDataForSend.push(item.x);
+    itemDataForSend.push(item.y);
+
+    itemsArray.push(itemDataForSend);
+  };
+
+  for(let [id, air] of gameObj.airMap) {
+    const airDataForSend = [];
+
+    airDataForSend.push(air.x);
+    airDataForSend.push(air.y);
+
+    airArray.push(airDataForSend);
+  }
+
+  return [playersArray, itemsArray, airArray];
+};
+
+
+//プレイヤーの接続が切れた時の処理
+function disconnect(socketId) {
+  //接続が切れた場合はゲーム離脱と判断し、playersMapからプレイヤーデータを削除する
+  gameObj.playersMap.delete(socketId);
+};
+
+
+//ミサイルアイテムをマップに追加する関数
+function addItem() {
+
+  //ミサイルアイテムの出現座標を設定
+  const itemX = Math.floor(Math.random() * gameObj.fieldWidth);
+  const itemY = Math.floor(Math.random() * gameObj.fieldHeight);
+  const itemKey = `${itemX},${itemY}`;
+
+  //ミサイルアイテムの出現位置が被ってしまった場合は、再度やり直す
+  if(gameObj.itemsMap.has(itemKey)) {
+    return addItem();
+  };
+
+  const itemObj = {
+    x: itemX,
+    y: itemY
+  };
+
+  //ミサイルアイテムの出現座標をキーとセットにしてgameObj.itemsMapに追加
+  gameObj.itemsMap.set(itemKey, itemObj);
+};
+
+
+//酸素アイテムをマップに追加する関数
+function addAir() {
+
+  //酸素アイテムの出現座標を設定
+  const airX = Math.floor(Math.random() * gameObj.fieldWidth);
+  const airY = Math.floor(Math.random() * gameObj.fieldHeight);
+  const airKey = `${airX},${airY}`;
+
+  //酸素アイテムの出現位置が被ってしまった場合は、再度やり直す
+  if(gameObj.airMap.has(airKey)){
+    return addAir();
+  };
+
+  const airObj = {
+    x: airX,
+    y: airY
+  };
+
+  //酸素アイテムの出現座標をキーとセットにしてgameObj.airMapに追加
+  gameObj.airMap.set(airKey, airObj);
+};
+
+
+//ここで作った関数をエキスポート
+module.exports = {
+  newConnection,
+  getMapData,
+  disconnect
+};
--- game_server/webSocketServer.js
+++ game_server/webSocketServer.js
@@ -16,17 +16,22 @@ function createWebSocketServer(io, game) {
 
     console.log('WebSocketのコネクションがありました。');  //ターミナルでの確認用
 
-    //start data という名前のデータを送信する
-    socket.emit('start data', {});
+    //WebSocketの通信ID,表示名,サムネイル画像のURLの３種をまとめたオブジェクトに、start data という名前をつけて送信
+    //game.newConnectionは、プレイヤーが新たにゲームに参加した時に実行する関数として、別で実装しておく
+    const startObj = game.newConnection(socket.id, displayName, thumbUrl);
+    socket.emit('start data', startObj);
 
     //ユーザが接続を切断をした時に実行
-    socket.on('disconnect', () => {});
+    socket.on('disconnect', () => {
+      //game.disconnectは、プレイヤーが接続を切った時に実行する関数として、別で実装
+      game.disconnect(socket.id);
+    });
   });
 
-  //66ミリ秒ごとに、map dataという名前のデータを送信
-  //volatile.emit...クライアントにデータが届いたどうかを確認しない送信方法。高頻度な通信を行う際に便利
+  //66ミリ秒ごとに、作成したマップデータにmap dataという名前をつけて送信
+  //volatile.emit...クライアントにデータが届いたどうかを確認しない送信方法。すぐに消える揮発性メッセージを送る。高頻度な通信を行う際に便利
   const socketTicker = setInterval(() => {
-    rootIo.volatile.emit('map data', {});
+    rootIo.volatile.emit('map data', game.getMapData());  //game.getMapDataはマップデータを作成する関数（別で実装）
   },
   66);
 };
