dcf9f0a7e4982a5262ce923002d36edc56d69065
Tanaka <f.tanaka@gftd.works>
2022-12-06 13:51:39 +0900

    ログインしない場合のプレイヤー名を変更（anonymous→ゲストユーザー）、ログインせずにプレイする場合のリンク文言変更

--- routes/game.js
+++ routes/game.js
@@ -4,8 +4,8 @@ const config = require('../config');
 
 router.get('/', (req, res, next) => {
 
-  let displayName = 'anonymous';  //アカウント名の初期値（匿名で参加する場合に使用される）
-  let thumbUrl = 'anonymous';  //サムネイル画像のパス（N予備校サイトによると未使用）
+  let displayName = 'ゲストユーザー';  //アカウント名の初期値（匿名で参加する場合に使用される）
+  let thumbUrl = '画像なし';  //サムネイル画像のパス（N予備校サイトによると未使用）
 
   //twitterとの連携後は、ゲーム画面にTwitterのアカウント名とサムネイル画像が表示されるようにする
   if(req.user){
