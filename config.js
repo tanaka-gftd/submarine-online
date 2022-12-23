6904f2f7101e03d355530c762a1af68942c39a7c
Tanaka <f.tanaka@gftd.works>
2022-12-23 11:17:49 +0900

    環境変数PRODUCTION_URLについての説明追加など

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 /* Twitterと連携したログインを行う場合は、このファイルにTwitterのAPIキーなどを記載してエキスポートする（今回は行わない） */
 
 
+/* PRODUCTION_URLが存在するなら、そちらを用い、存在しないなら開発用URLを使用する */
+/* PRODUCTION_URLは、renderのDashboardより環境変数として設定しておく */
 const url = process.env.PRODUCTION_URL || 'http://localhost:8000';
 
 module.exports = {
-  /* 開発用ページのURLもここに記載しておくと、修正しやすく便利 */
   ipAddress: url
-  //ipAddress: 'https://tanaka-submarine-online.onrender.com'
 };
