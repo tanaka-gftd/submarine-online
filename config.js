3be94b2a77611f4d9dafa39a992d0190c8f4dc84
Tanaka <f.tanaka@gftd.works>
2022-12-23 11:13:27 +0900

    render環境下を示す変数名を変更

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 /* Twitterと連携したログインを行う場合は、このファイルにTwitterのAPIキーなどを記載してエキスポートする（今回は行わない） */
 
 
-const url = process.env.URL || 'http://localhost:8000';
+const url = process.env.PRODUCTION_URL || 'http://localhost:8000';
 
 module.exports = {
   /* 開発用ページのURLもここに記載しておくと、修正しやすく便利 */
